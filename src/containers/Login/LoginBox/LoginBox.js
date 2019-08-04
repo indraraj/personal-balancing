14fcff1678d718a5c1b9a421a7e27716cecda594
indra raj shukla <indraraj14@gmail.com>
2019-08-04 12:05:12 +0530

    login Validation

--- src/containers/Login/LoginBox/LoginBox.js
+++ src/containers/Login/LoginBox/LoginBox.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useRef} from "react";
 import WhiteBox from '../../../helpers/WhiteBox/WhiteBox';
 import LoginInput from '../components/LoginInput/LoginInput';
 import {withRouter} from 'react-router-dom';
@@ -9,14 +9,27 @@ import './LoginBox.css'
 
 const LoginBox = (props) =>{
     const authContext = useContext(AuthContext);
+    const errorMsgRef = useRef(null);
+    const [userEmail,setUserEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const userLoginHandlerBtn = () =>{
+        if((userEmail === 'indra' || userEmail === 'riya') && password === 'ihl')
+            authContext.userLoginHandler(authContext.loginState)
+        else
+            errorMsgRef.current.style.display = 'block';
+    }
+    const userEntryFocus = (e) =>{
+        errorMsgRef.current.style.display = 'none';
+    }
     return(
         <WhiteBox>
             <div className='LoginBox'>
                 <h2>Log into Kazerian Box</h2>
                 <h4>or <u className='CreateAccountLink' onClick={props.showSignin}>Create Account</u></h4>
-                <LoginInput placeholderText='Email Address' inputType='email'></LoginInput>
-                <LoginInput placeholderText='Password' inputType='password'></LoginInput>
-                <LoginFormButton loginFormBtnHandler={() => authContext.userLoginHandler(authContext.loginState)}>Log In</LoginFormButton>
+                <LoginInput placeholderText='Email Address' userFocus={userEntryFocus} name='userEmail' inputfeild={userEmail} inputfeildSetter={setUserEmail} inputType='email'></LoginInput>
+                <LoginInput placeholderText='Password' userFocus={userEntryFocus} name='password' inputfeild={password} inputfeildSetter={setPassword} inputType='password'></LoginInput>
+                <p className='errorMsg' ref={errorMsgRef} > Incorrect email or password, please try again.</p>
+                <LoginFormButton loginFormBtnHandler={userLoginHandlerBtn}>Log In</LoginFormButton>
                 <p> or continue with.</p>
                 <SocialMedia></SocialMedia>
                 <p className='LoginDisclaimer'>Secure login for extra security with kazerian love.</p>
